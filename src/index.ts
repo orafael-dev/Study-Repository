bc582052b141feefbcbe2ef34f204f2acd6df3ca
Rafael Almeida <almeidarp.dev@gmail.com>
2024-08-06 09:40:12 -0300

    Exercicio Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
-type Planet= "Mercúrio" | "Vênus" | "Terra" | "Marte" | "Júpiter" | "Saturno" | "Urano" | "Netuno" | "Plutão"
+type PlanetSituation = "Habitado" | "Habitável" | "Inabitável" | "Inexplorado";
 
-let planet: Planet
+type PlanetCoordinates = [number, number, number, number];
 
+type Planet = {
+  name: string;
+  coordinates: PlanetCoordinates;
+  situation: PlanetSituation;
+  satellites: string[];
+};
 
-let homePlanet: Planet
+const planets: Planet[] = [];
 
-function checkPlanet(planet: Planet) {
-    if (planet === "Terra") {
-        console.log("Estamos na Terra!")
-    }
+function addPlanet(
+  name: string,
+  coordinates: PlanetCoordinates,
+  situation: PlanetSituation
+) {
+  planets.push({
+    name,
+    coordinates,
+    situation,
+    satellites: [],
+  });
 
+  alert(`O planela ${name} foi registrado com sucesso`);
+}
+
+function findPlanet(name: string) {
+  const planet = planets.find((planet) => planet.name === name);
+
+  return planet ?? false;
+}
+
+function updateSituation(situation: PlanetSituation, planet: Planet) {
+  planet.situation = situation;
+
+  alert(
+    `A situação do planeta ${planet.name} foi atualizada para ${situation}`
+  );
+}
+
+function addSatellites(name: string, planet: Planet) {
+  planet.satellites.push(name);
+
+  alert(`O satélite ${name} foi adicionado ao planeta ${planet.name}`);
+}
+
+function removeSatellite(name: string, planet: Planet) {
+  planet.satellites = planet.satellites.filter(
+    (satellite) => satellite !== name
+  );
+
+  alert(`O satélite ${name} foi removido do planeta ${planet.name}`);
 }
