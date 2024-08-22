bb33e317c2fe8a398a078d643c922e00a815dc5b
Rafael Almeida <almeidarp.dev@gmail.com>
2024-08-21 23:08:40 -0300

    estudos

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -1,38 +1,37 @@
-/* eslint-disable react/prop-types */
-function OrderDetails({itemsInBag}) {
-  
-  function CalculateTotal() {
-    let orderTotal = 0
-    itemsInBag.forEach(item => orderTotal += (item.quantity * item.price))
-    return orderTotal
-  }
-    return(
-        <>
-        <section className="summary">
-        <strong>Order SUmmary</strong>
-        <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-          {itemsInBag.map(item => 
-            <tr key={item.id}>
-              <td>{item.quantity} x {item.name}</td>
-              <td>$ {(item.quantity * item.price).toFixed(2)}</td>
-            </tr>
-          
-          )}
+function OrderDetails ({itemsInBag}) {
 
-            <tr>
-              <th>Total</th>
-              <th>$ {CalculateTotal().toFixed(2)}</th>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+    function calculateTotal(){
+        let orderTotal = 0;
+        itemsInBag.forEach(item => orderTotal += item.price * item.quantity);
+        return orderTotal.toFixed(2);
+    }
+
+    return ( 
+        <>
+            <section className="summary">
+                <strong>Order Summary</strong>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    { itemsInBag.map(item => 
+                        <tr>
+                            <td>{item.quantity}x {item.name}</td>
+                            <td>$ {(item.price * item.quantity).toFixed(2)}</td>
+                        </tr>
+                    ) }
+                
+                        <tr>
+                            <th>Total</th>
+                            <th>$ {calculateTotal()}</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
         </>
     )
 }
